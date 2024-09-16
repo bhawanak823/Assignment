446e6415f8fd632598305a5b9c64fe005e6f6775
bhawana khatiwada <bhawana.khatiwada@metropolia.fi>
2024-09-16 17:27:36 +0300

    i have submited week 2 homework up to task 4

---
+++ week2.py/task3.py
@@ -0,0 +1,12 @@
+#ask the user for the length and width of the rectangle
+length=float(input("Enter the length of the rectangle:"))
+width=float(input("Enter the width of the rectangle"))
+
+#calculate the perimeter
+perimeter = 2*(length+width)
+
+#calculate the area
+area = (length*width)
+#print
+print(f"print the perimeter of the rectangle is {perimeter}")
+print(f"print the area of rectangle is {area}")
