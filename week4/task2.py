d9d9140409eabc6b56f404319668baa6fff7195c
bhawana khatiwada <bhawana.khatiwada@metropolia.fi>
2024-09-24 09:37:34 +0300

    i have done task1 of week4

---
+++ week4/task2.py
@@ -0,0 +1,7 @@
+# Function to convert inches to centimeters
+def inches_to_cm(inches):
+    return inches * 2.5
+inches = float(input("Enter a value in inches (negative to stop): "))
+print(inches_to_cm(inches))
+
+
